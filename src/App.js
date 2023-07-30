38acfe1a16618b7abaaec91a50f76aa7f44061b3
BakedBean27 <21blm9@queensu.ca>
2023-07-31 00:04:34 +1200

    Project initialization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Atom from "./Atom.js";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Atom />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +20,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Atom.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const Atom = () => {
+  return <img src={logo} className="App-logo" alt="logo" />;
+};
+
+export default Atom;
